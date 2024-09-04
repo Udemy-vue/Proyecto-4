3ca1b612286e05e423546fd101f6cece62f549ba
ndcontrerasr <nico.contreramirez@gmail.com>
2024-09-04 18:30:44 -0500

    Correccion 2

--- src/store/bundle.js
+++ src/store/bundle.js
@@ -18,7 +18,7 @@ export const useUserStore = defineStore("user", {
       userData: "bluuweb",
       userInfo: {},
       loadingUser: false,
-      loading: false,
+      loading: true,
       loadingSession: false,
       selectedKeys: ['2']
   }),
